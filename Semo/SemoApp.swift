de12adb9b7a21675078767afc26c039ef7ee980c
Thina <frog7134@gmail.com>
2022-07-16 14:59:36 +0900

    Initial Commit

---
+++ Semo/SemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemoApp.swift
+//  Semo
+//
+//  Created by 유정인 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct SemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
