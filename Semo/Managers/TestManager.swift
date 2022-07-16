573475014c8ce6fb37fd5e0e8d67c48b084af1f2
Terry-Koo <xpflxhfl94@naver.com>
2022-07-16 15:11:16 +0900

    프로젝트 폴더 구조 추가

---
+++ Semo/Managers/TestManager.swift
@@ -0,0 +1,8 @@
+//
+//  TestManager.swift
+//  Semo
+//
+//  Created by Terry Koo on 2022/07/16.
+//
+
+import Foundation
---
+++ Semo/Models/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestModel.swift
+//  Semo
+//
+//  Created by Terry Koo on 2022/07/16.
+//
+
+import Foundation
