302d0909676d032618f82327c5eda923a6823f45
Anti9uA <didlaak6000@naver.com>
2022-07-17 16:41:11 +0900

    [feat] #5 노래 추가 뷰 텍스트 필드 및 버튼 구현

---
+++ Semo/Extensions/ViewExtension.swift
@@ -0,0 +1,46 @@
+//
+//  ViewExtension.swift
+//  SingSangSong
+//
+//  Created by Kyubo Shim on 2022/07/17.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    
+    // MARK: 텍스트 필드 관련 함수들
+    
+    /// 텍스트 필드 밑줄 커스텀 및 입력 여부에 따라 색이 변화합니다.
+    /// - Parameter isEditing: 텍스트 필드 편집 여부 확인
+    /// - Parameter isFull: 텍스트 필드 공백 여부 확인
+    /// - Returns: 텍스트 필드 뷰
+    func underlineTextField(isEditing : Bool, isFull: Bool) -> some View {
+        var lineColor: Color = Color.grayScale4
+        if isEditing == true {
+            lineColor = Color.mainPurpleColor
+        }
+        if isFull == true && isEditing == false {
+            lineColor = Color.grayScale2
+        }
+        return self
+            .padding(.vertical, 10)
+            .overlay(Rectangle().frame(height: 2).padding(.top, 35))
+            .padding(10)
+            .foregroundColor(lineColor)
+    }
+    
+    /// Placeholder의 색을 커스텀 해주는 함수입니다.
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment) {
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
+        }
+    
+}
---
+++ Semo/Views/ConfirmButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  ConfirmButtonView.swift
+//  SingSangSong
+//
+//  Created by Kyubo Shim on 2022/07/17.
+//
+
+import SwiftUI
+
+/// 확인 및 완료 버튼을 위한 라벨 서브뷰 입니다.
+/// - 라벨 이름만 생성자로 넣어서 쓰시면 됩니다.
+struct ConfirmButtonView: View {
+    var buttonName: String
+    init(buttonName: String) {
+        self.buttonName = buttonName
+    }
+    var body: some View {
+        Text(buttonName)
+            .font(.system(size: 16, weight: .semibold))
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 350, height: 48)
+            .background(Color.mainPurpleColor)
+            .cornerRadius(16)
+    }
+}
+
+struct ConfirmButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmButtonView(buttonName: "확인")
+    }
+}
