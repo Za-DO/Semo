76d60ab88b81c71fb94c550ec697f7e026b3c1dc
Anti9uA <didlaak6000@naver.com>
2022-07-23 17:51:15 +0900

    [feat] #3 Picker 테스트

---
+++ Pods/Target Support Files/SwiftUIWheelPicker/SwiftUIWheelPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftUIWheelPicker : NSObject
+@end
+@implementation PodsDummy_SwiftUIWheelPicker
+@end
--- Semo/Views/SongDetail/LevelPickerView.swift
+++ Semo/Views/SongDetail/LevelPickerView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftUIWheelPicker
 
 struct LevelPickerView: View {
+    @State var indexBasic: Int = 1
+    @State var items: [String] = ["상", "중", "하"]
     var body: some View {
         VStack {
             HStack {
@@ -17,6 +20,20 @@ struct LevelPickerView: View {
             
             // TODO: - pickerView 가져오기
             // pickerView가 들어갈 부분
+
+            SwiftUIWheelPicker($indexBasic, items: $items) { value in
+                GeometryReader { reader in
+                    Text("\(value)")
+                        .frame(width: reader.size.width, height: reader.size.height,
+                               alignment: .center)
+                        .font(.system(size: 50))
+                }
+            }
+            .scrollAlpha(0.2)
+            .frame(width: UIScreen.main.bounds.width, height: 70, alignment: .center)
+            .padding(.horizontal, -20)
+            
+            
             Spacer()
                 .frame(height: 102)
         }
