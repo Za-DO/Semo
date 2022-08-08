51632dc25171618956ca2a911f512376e92bd355
Terry-Koo <xpflxhfl94@naver.com>
2022-08-09 08:00:19 +0900

    [feat] #27 TextFieldView에 입력되었을때 UI 구현

--- Semo/Views/SingingList/SingingListSheet/SingingListSheetView.swift
+++ Semo/Views/SingingList/SingingListSheet/SingingListSheetView.swift
@@ -8,20 +8,22 @@
 import SwiftUI
 // TODO: 커스텀 sheet로 바꿔줘야 함
 struct SingingListSheetView: View {
-    @State var newLSingingListTitle: String = ""
+    @State var newSingingListTitle: String = ""
+    @State private var singingListToggle: [Bool] = Array(repeating: false, count: 20)
     var body: some View {
         ZStack {
             Color.grayScale5.ignoresSafeArea()
             VStack{
-                TextFieldView(text: $newLSingingListTitle, placeholder: "새로운 리스트를 바로 추가해보세요.")
+                // TODO: newSingingListTitle지워지면 포커싱 풀리게
+                TextFieldView(text: $newSingingListTitle, placeholder: "새로운 리스트를 바로 추가해보세요.")
                     .padding(.horizontal, 20)
                 ScrollView{
-                    SingingListToggleView()
+                    SingingListToggleView(toggleArray: $singingListToggle, newSingingListTitle: $newSingingListTitle)
                 }
                 Button(action: {
                     print("button")
                 }, label: {
-                    FinalConfirmButtonView(buttonName: "확인")
+                    FinalConfirmButtonView(buttonName: newSingingListTitle.isEmpty ? "확인" : "리스트 추가하기")
                 })
             }
             .padding(EdgeInsets(top: 40, leading: 0, bottom: 37, trailing: 0))
--- Semo/Views/SingingList/SingingListSheet/SingingListToggleView.swift
+++ Semo/Views/SingingList/SingingListSheet/SingingListToggleView.swift
@@ -8,21 +8,25 @@
 import SwiftUI
 
 struct SingingListToggleView: View {
-    @State private var toggleArray: [Bool] = Array(repeating: false, count: 20)
+    @Binding var toggleArray: [Bool]
+    @Binding var newSingingListTitle: String
 
     var body: some View {
         VStack(spacing: 0){
             ForEach(0..<20){ index in
                 Toggle("빠른 싱잉리스트", isOn: $toggleArray[index])
                     .toggleStyle(CheckboxToggleStyle())
+                    .onChange(of: toggleArray, perform: {newValue in
+                        newSingingListTitle = ""
+                    })
            }
        }
     }
 }
 
 
-struct SingingListToggleView_Previews: PreviewProvider {
-    static var previews: some View {
-        SingingListToggleView()
-    }
-}
+//struct SingingListToggleView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SingingListToggleView()
+//    }
+//}
