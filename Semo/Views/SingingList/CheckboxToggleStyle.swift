50091b8c8777383722990e406babe34d15198893
Terry-Koo <xpflxhfl94@naver.com>
2022-08-09 02:46:23 +0900

    [Feat] #27 커스텀 토글 스타일 구현

---
+++ Semo/Views/SingingList/CheckboxToggleStyle.swift
@@ -0,0 +1,49 @@
+//
+//  CheckboxToggleStyle.swift
+//  Semo
+//
+//  Created by Terry Koo on 2022/08/09.
+//
+import SwiftUI
+
+struct CheckboxToggleStyle: ToggleStyle {
+    private enum Config {
+        static let iconSize: CGFloat = 22
+        static let animationDuration: TimeInterval = 0.15
+        static let onIconName = "checkmark.circle.fill"
+        static let offIconName = "circle"
+    }
+
+    var onColor: Color = .white
+    var offColor: Color = .white
+
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.label
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(Color.white)
+            Spacer()
+            ZStack {
+                // off
+                Image(systemName: Config.offIconName)
+                    .resizable()
+                    .foregroundColor(offColor)
+                // on
+                Image(systemName: Config.onIconName)
+                    .resizable()
+                    .foregroundColor(onColor)
+                    .opacity(configuration.isOn ? 1 : 0)
+            }
+            .frame(width: Config.iconSize, height: Config.iconSize)
+            .animation(.easeInOut(duration: Config.animationDuration))
+        }
+        .padding(EdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 40))
+        .frame(height: 50)
+        .background(configuration.isOn ? Color.grayScale2 : Color.grayScale5)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("tab")
+            configuration.isOn.toggle()
+        }
+    }
+}
