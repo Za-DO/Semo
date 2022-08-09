69251afa3c587b02cd9d9aa72f345ab4d42bd6aa
Terry-Koo <xpflxhfl94@naver.com>
2022-08-09 18:56:14 +0900

    [style] 컨벤션 수정

--- Semo/Views/SongDetail/SingingListTagView.swift
+++ Semo/Views/SongDetail/SingingListTagView.swift
@@ -21,24 +21,6 @@ struct SingingListTagView: View {
             }
             
             // TODO: - 싱잉리스트 데이터 받아오기
-//            ForEach() {
-//                HStack {
-//                    Button {
-//                        print("싱잉리스트 태그 삭제하기")
-//                    } label: {
-//                        Image(systemName: "minus.circle.fill")
-//                            .foregroundColor(.grayScale4)
-//                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 10))
-//                    }
-//                    HStack {
-//                        Text("aa")
-//                            .font(.system(size: 20, weight: .semibold))
-//                            .foregroundColor(.white)
-//                    }
-//                    Spacer()
-//                }
-//                .padding(.top, 20)
-//            }
             ForEach(singingList){
                 singingListTag(title: $0.title ?? "제목 없음")
             }
