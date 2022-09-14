f33eebd02585c5fe83b1f2b11aa2da1f8f6937d7
Terry-Koo <xpflxhfl94@naver.com>
2022-09-14 17:59:29 +0900

    [feat] #61 saveNewSong(), saveNewSingingList()메서드에서 데이터 중복 검사

--- Semo/Managers/CoreDataManager.swift
+++ Semo/Managers/CoreDataManager.swift
@@ -14,27 +14,47 @@ class CoreDataManager {
     
     private init() {}
     
+    // MARK: - 새로운 노래 저장
     func saveNewSong(songTitle: String, songSinger: String) {
-        let newSong: Song = Song(context: self.viewContext)
-        newSong.timestamp = Date()
-        newSong.id = UUID()
-        newSong.title = songTitle
-        newSong.singer = songSinger
         do {
-            try viewContext.save()
+            let fetchRequest = NSFetchRequest<Song>(entityName: "Song")
+            fetchRequest.predicate = NSPredicate(format: "title == %@ && singer == %@", songTitle, songSinger)
+            let fetchedResults = try viewContext.fetch(fetchRequest)
+            // 중복 검사
+            guard (fetchedResults.first?.title) != nil  else {
+                let newSong: Song = Song(context: self.viewContext)
+                newSong.timestamp = Date()
+                newSong.id = UUID()
+                newSong.title = songTitle
+                newSong.singer = songSinger
+                try viewContext.save()
+                print("title : \(songTitle), singer : \(songSinger) 저장 완료")
+                return
+            }
+            print("[중복 노래] title : \(songTitle), singer : \(songSinger) 저장 불가")
         } catch {
             print(error.localizedDescription)
         }
     }
     
+    // MARK: - 새로운 싱잉리스트 생성
     func saveNewSingingList(singingListTitle: String) {
-        let newSingingList: SingingList = SingingList(context: self.viewContext)
-        newSingingList.timestamp = Date()
-        newSingingList.id = UUID()
-        newSingingList.title = singingListTitle
-        newSingingList.count = 0
         do {
-            try viewContext.save()
+            let fetchRequest = NSFetchRequest<SingingList>(entityName: "SingingList")
+            fetchRequest.predicate = NSPredicate(format: "title == %@", singingListTitle)
+            let fetchedResults = try viewContext.fetch(fetchRequest)
+            // 중복 검사
+            guard (fetchedResults.first?.title) != nil else {
+                let newSingingList: SingingList = SingingList(context: self.viewContext)
+                newSingingList.timestamp = Date()
+                newSingingList.id = UUID()
+                newSingingList.title = singingListTitle
+                newSingingList.count = 0
+                try viewContext.save()
+                print("title : \(singingListTitle) 싱잉리스트 생성 완료")
+                return
+            }
+            print("[중복 싱잉리스트] title : \(singingListTitle) 싱잉리스트 생성 불가")
         } catch {
             print(error.localizedDescription)
         }
