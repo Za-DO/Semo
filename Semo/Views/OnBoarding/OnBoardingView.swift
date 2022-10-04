413924d965f8a75bd4e2d7c3fadc17901b4b1cf6
Eunbi Cho <moomb70@gmail.com>
2022-10-04 21:36:43 +0900

    [feat] #110 기기대응을 위한 폰트, 패딩값 수정 함수 파일 추가, 온보딩 파일에서 익스텐션 삭제

--- Semo/Views/OnBoarding/OnBoardingView.swift
+++ Semo/Views/OnBoarding/OnBoardingView.swift
@@ -60,25 +60,6 @@ struct OnBoardingView: View {
     
 }
 
-
-extension UIScreen {
-    
-    static let screenWidth = UIScreen.main.bounds.size.width
-    static let screenHeight = UIScreen.main.bounds.size.height
-    static let screenSize = UIScreen.main.bounds.size
-
-    static func getWidth(_ width: CGFloat) -> CGFloat {
-        screenWidth / 390 * width
-    }
-
-    static func getHeight(_ height: CGFloat) -> CGFloat {
-        screenHeight / 844 * height
-    }
-}
-
-
-
-
 struct OnBoardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnBoardingView()
